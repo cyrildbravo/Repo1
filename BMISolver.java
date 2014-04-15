620800e398a51e6e65403ffd02392db202384f7b
yssamarie <yssamarie.v@gmail.com>
2014-04-15 13:00:12 +0800

    Modified ui and solver

--- BMISolver.java
+++ BMISolver.java
@@ -4,10 +4,10 @@ import javax.swing.*;
 import java.util.*;
 
 public class BMISolver implements ActionListener{
-	private BMIUI ui;
 	private String name;
 	private int age;
 	private double weight, height, bmi = 0.0;
+	private BMIUI ui;
 
 	public BMISolver(BMIUI ui){
 		this.ui = ui;
@@ -15,15 +15,16 @@ public class BMISolver implements ActionListener{
 	}
 
 	public void actionPerformed(ActionEvent e){
+		
+		// Prints results to BMIUI when button is clicked
 		if(e.getSource() == ui.getButton()){
-			System.out.println("BUTTON");
 			try{
 				name = ui.getText(0).trim();
 				age = Integer.parseInt(ui.getText(1).trim());
 				weight = Double.parseDouble(ui.getText(2).trim());
 				height = Double.parseDouble(ui.getText(3).trim());
 				bmi = weight+height;//weight / (height * height);
-				ui.setText("BMI: \n" + bmi);
+				ui.setText("Hi " + name +"!\nYour BMI is " + bmi + ".\n");
 			}
 			catch(Exception ex){
 				JOptionPane.showMessageDialog(null, "Invalid input.");
