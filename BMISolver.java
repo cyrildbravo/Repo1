3e55c51aa4eb165c6427ffb73c67a14e4c60c2f6
cyrildbravo <cyrildbravo@gmail.com>
2014-04-15 13:34:57 -0700

    Delete BMISolver.java.orig

--- BMISolver.java
+++ BMISolver.java
@@ -6,10 +6,10 @@ import java.io.*;
 
 public class BMISolver implements ActionListener{
 	/* Initialize global variables */
-	private BMIUI ui;
 	private String name;
 	private int age;
 	private double weight, height, bmi = 0.0;
+	private BMIUI ui;
 
 	/* Constructor for BMISolver */
 	public BMISolver(BMIUI ui){
@@ -19,15 +19,15 @@ public class BMISolver implements ActionListener{
 	}
 
 	public void actionPerformed(ActionEvent e){
+		// Prints results to BMIUI when button is clicked
 		if(e.getSource() == ui.getButton()){
-			System.out.println("BUTTON");
 			try{
 				name = ui.getText(0).trim();
 				age = Integer.parseInt(ui.getText(1).trim());
 				weight = Double.parseDouble(ui.getText(2).trim());
 				height = Double.parseDouble(ui.getText(3).trim());
 				bmi = weight+height;//weight / (height * height);
-				ui.setText("BMI: \n" + bmi);
+				ui.setText("Hi " + name +"!\nYour BMI is " + bmi + ".\n");
 			}
 			catch(Exception ex){
 				JOptionPane.showMessageDialog(null, "Invalid input.");
--- BMIUI.java
+++ BMIUI.java
@@ -3,50 +3,43 @@ import java.awt.*;
 import javax.swing.*;
 
 public class BMIUI{
-	/* Initialize global variables */
 	private static JFrame frame = new JFrame("BMI");
 	private static JPanel panel = new JPanel();
-	private static JButton button = new JButton();
 	private static JLabel[] label = new JLabel[4];
 	private static JTextArea[] textArea= new JTextArea[5];
+	private static JButton button;
 	private static Toolkit kit = Toolkit.getDefaultToolkit();
 	private static String[] labelHeader = {"Enter your name:", "Enter your age:", "Enter your weight (kg):", "Enter your height (cm):" };
 	private static int height, width;
-	public BMISolver solver = new BMISolver(this);
-	private Font font = new Font("Arial", Font.BOLD, 12);
-	private Font font2 = new Font("Comic Sans MS", Font.BOLD, 18);
+	private static BMISolver solver;
 	
-	/* Constructor for BMIUI */
+	// Constructor for BMIUI
 	public BMIUI(){
 		height = kit.getScreenSize().height;
 		width = kit.getScreenSize().width;
 		setComponents();
 		setFrame();
+		solver = new BMISolver(this);
 	}
 	
-	/* setComponents: sets buttons, labels, textarea into JPanel */
+	// Sets buttons, labels, text areas in JFrame
 	public void setComponents(){
 		panel.setLayout(null);
 		
 		for(int i=0, j=50; i<4; i++, j+=50){
 			label[i] = new JLabel(labelHeader[i]);
-			label[i].setFont(font);
 			label[i].setBounds(30,j,130,50);
-			panel.add(label[i]);
-			
 			textArea[i] = new JTextArea();
-			textArea[i].setFont(font2);
-			textArea[i].setBounds(180,j+10,150,30);
+			textArea[i].setBounds(170,j+15,150,20);
+			panel.add(label[i]);
 			panel.add(textArea[i]);
 		}
 
 		button = new JButton("Compute BMI");
-		button.setFont(font);
 		button.setBounds(130,270,120,30);
 		panel.add(button);
 		
 		textArea[4] = new JTextArea();
-		textArea[4].setFont(font2);
 		textArea[4].setEnabled(false);
 		textArea[4].setBounds(360,50,300,250);
 		panel.add(textArea[4]);
